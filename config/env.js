a119891cb3f1a15064ce17ff5aed73dc188d5e34
Franklin Clark <franklin@modeanalytics.com>
2019-10-27 19:49:59 -0700

    Enable now dev

--- config/env.js
+++ config/env.js
@@ -1,6 +1,6 @@
 const path = require('path');
 
 require('dotenv').config({
-  path: path.resolve(`${__dirname}/../`, '.env.local'),
+  path: path.resolve(`${__dirname}/../`, '.env.build'),
   debug: process.env.DEBUG,
 });
