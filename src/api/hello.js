3776ca4c77031cf2eac7a98ec792b582790440cd
Franklin Clark <franklin@modeanalytics.com>
2019-11-02 23:05:56 -0700

    Setup netlify

---
+++ src/api/hello.js
@@ -0,0 +1,7 @@
+// eslint-disable-next-line import/prefer-default-export
+export async function handler() {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ msg: 'Hello, World!' }),
+  };
+}
