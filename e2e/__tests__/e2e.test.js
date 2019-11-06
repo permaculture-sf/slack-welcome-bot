0353b8f490faf8644d07008fa4758f5291240c1d
Franklin Clark <franklin@modeanalytics.com>
2019-11-05 19:09:28 -0800

    Remove hello world endpoint

--- e2e/__tests__/e2e.test.js
+++ e2e/__tests__/e2e.test.js
@@ -1,33 +1,9 @@
 const test = require('tape-async');
 const kill = require('tree-kill');
-const debug = require('debug')('e2e:smoke');
 const request = require('supertest');
 const { startServer } = require('../util');
 
 require('debug').enable('e2e:*');
-
-debug('Starting');
-
-test('app exists', (t) => new Promise(() => {
-  let serverProcessId;
-
-  startServer().then((pid) => {
-    serverProcessId = pid;
-    request('http://localhost:8888')
-      .get('/.netlify/functions/hello')
-      .expect('Content-Type', /json/)
-      .expect(200)
-      .expect((res) => {
-        t.deepEqual(JSON.parse(res.text), {
-          msg: 'Hello, World!',
-        });
-      })
-      .end((err) => {
-        kill(serverProcessId);
-        t.end(err);
-      });
-  });
-}));
 
 test('heartbeat endpoint', (t) => new Promise(() => {
   let serverProcessId;
@@ -37,6 +13,7 @@ test('heartbeat endpoint', (t) => new Promise(() => {
     request('http://localhost:8888')
       .get('/.netlify/functions/heartbeat')
       .expect(200)
+      .expect('Content-Type', /json/)
       .expect((res) => {
         t.deepEqual(JSON.parse(res.text), {
           msg: true,
