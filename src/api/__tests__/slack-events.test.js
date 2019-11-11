127613fd80806145ab1fae16caf621f6a9067d15
Franklin Clark <franklin@modeanalytics.com>
2019-11-10 21:39:39 -0800

    Return challenge

--- src/api/__tests__/slack-events.test.js
+++ src/api/__tests__/slack-events.test.js
@@ -1,7 +1,7 @@
 const test = require('tape-async');
 const { handler } = require('../slack-events');
 
-test('slack/events', async (t) => {
+test('slack-events', async (t) => {
   let response;
 
   try {
@@ -9,6 +9,38 @@ test('slack/events', async (t) => {
     t.ok(response, 'returns response');
     t.equal(response.statusCode, 200, 'events status');
     t.equal(response.body, '{"msg":true}', 'events status');
+    t.equal(
+      response.headers['Content-Type'],
+      'application/json',
+      'json header',
+    );
+    t.end();
+  } catch (e) {
+    t.end(e);
+  }
+});
+
+test('slack-events', async (t) => {
+  let response;
+  const request = {
+    queryStringParameters: {
+      type: 'url_verification',
+      token: 'sn9B4G2rxxxxxxxxxxxxxxxx',
+      challenge: '7X8z79kUqugwJuhry5xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+    },
+  };
+
+  try {
+    response = await handler(request);
+    t.ok(response, 'returns response');
+    t.equal(response.statusCode, 200, 'events status');
+    t.deepEqual(
+      JSON.parse(response.body),
+      {
+        accepted: '7X8z79kUqugwJuhry5xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+      },
+      'events status',
+    );
     t.equal(
       response.headers['Content-Type'],
       'application/json',
--- src/api/slack-events.js
+++ src/api/slack-events.js
@@ -1,8 +1,11 @@
 // eslint-disable-next-line import/prefer-default-export
-async function handler() {
+async function handler(event) {
+  const params = event && event.queryStringParameters;
+  const challenge = params ? params.challenge : false;
+
   return {
     statusCode: 200,
-    body: JSON.stringify({ msg: true }),
+    body: JSON.stringify(challenge ? { accepted: challenge } : { msg: true }),
     headers: {
       'Content-Type': 'application/json',
     },
