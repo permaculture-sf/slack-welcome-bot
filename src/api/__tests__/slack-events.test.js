8fb582f01804b8c1017632833f091c42ff8902bb
Franklin Clark <franklin@modeanalytics.com>
2019-11-13 22:44:33 -0800

    Refactor tests

--- src/api/__tests__/slack-events.test.js
+++ src/api/__tests__/slack-events.test.js
@@ -2,53 +2,30 @@ const test = require('tape-async');
 const { handler } = require('../slack-events');
 
 test('slack-events default', async (t) => {
-  let response;
+  const response = await handler();
 
-  try {
-    response = await handler();
-    t.ok(response, 'returns response');
-    t.equal(response.statusCode, 200, 'events status');
-    t.equal(response.body, '{"msg":true}', 'events status');
-    t.equal(
-      response.headers['Content-Type'],
-      'application/json',
-      'json header',
-    );
-    t.end();
-  } catch (e) {
-    t.end(e);
-  }
+  t.ok(response, 'returns response');
+  t.equal(response.statusCode, 200, 'events status');
+  t.equal(response.body, '{"msg":true}', 'events status');
+  t.equal(response.headers['Content-Type'], 'application/json', 'json header');
+  t.end();
 });
 
 test('slack-events url_verification', async (t) => {
-  let response;
+  const mockChallenge = '7X8z79kUqugwJuhry5xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx';
   const request = {
     httpMethod: 'POST',
     body: JSON.stringify({
       type: 'url_verification',
       token: 'sn9B4G2rxxxxxxxxxxxxxxxx',
-      challenge: '7X8z79kUqugwJuhry5xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+      challenge: mockChallenge,
     }),
   };
+  const response = await handler(request);
 
-  try {
-    response = await handler(request);
-    t.ok(response, 'returns response');
-    t.equal(response.statusCode, 200, 'events status');
-    t.deepEqual(
-      JSON.parse(response.body),
-      {
-        accepted: '7X8z79kUqugwJuhry5xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
-      },
-      'events status',
-    );
-    t.equal(
-      response.headers['Content-Type'],
-      'application/json',
-      'json header',
-    );
-    t.end();
-  } catch (e) {
-    t.end(e);
-  }
+  t.ok(response, 'returns response');
+  t.equal(response.statusCode, 200, 'events status');
+  t.equal(JSON.parse(response.body).accepted, mockChallenge, 'events status');
+  t.equal(response.headers['Content-Type'], 'application/json', 'json header');
+  t.end();
 });
