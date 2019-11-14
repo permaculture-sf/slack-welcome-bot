948611ce5990795c740f8211272dec81d91a5aaf
Franklin Clark <franklin@modeanalytics.com>
2019-11-13 23:23:16 -0800

    Add post assertion method

--- src/lib/__tests__/request-event.test.js
+++ src/lib/__tests__/request-event.test.js
@@ -1,7 +1,13 @@
 const test = require('tape-async');
 const RequestEvent = require('../request-event');
 
-test('request event instantiates', (t) => {
+test('RequestEvent instantiates', (t) => {
   t.ok(new RequestEvent({}), 'instantiates');
+  t.end();
+});
+
+test('RequestEvent.isPost()', (t) => {
+  const event = new RequestEvent({ httpMethod: 'POST' });
+  t.equal(event.isPost(), true, 'assert post method');
   t.end();
 });
--- src/lib/request-event.js
+++ src/lib/request-event.js
@@ -4,6 +4,10 @@ class RequestEvent {
   constructor(event) {
     this.event = produce(event, (draft) => draft);
   }
+
+  isPost() {
+    return this.event.httpMethod === 'POST';
+  }
 }
 
 module.exports = RequestEvent;
