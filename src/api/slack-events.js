c3518223c6c932817adc85d72d5f5ec234df5f77
Franklin Clark <franklin@modeanalytics.com>
2019-11-17 09:52:19 -0800

    Log non challenge events

--- src/api/slack-events.js
+++ src/api/slack-events.js
@@ -1,16 +1,26 @@
 const RequestEvent = require('../lib/request-event');
+const SlackEvent = require('../lib/slack-event');
 
 // eslint-disable-next-line import/prefer-default-export
 async function handler(event) {
   const requestEvent = new RequestEvent(event);
   const data = event && requestEvent.isPost() && requestEvent.parsedBody();
+  const slackEvent = new SlackEvent(data);
+
+  if (slackEvent.isChallenge()) {
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ accepted: data.challenge }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
+  }
+  console.log(event.body);
 
   return {
     statusCode: 200,
-    body: JSON.stringify({ accepted: data.challenge }),
-    headers: {
-      'Content-Type': 'application/json',
-    },
+    body: '',
   };
 }
 
